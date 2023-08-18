b4cc94c8b5b7f19314eb41d65155882d28aae402
LuciaYuhasz <luciayuhasz@gmail.com>
2023-08-18 11:05:54 -0300

    cambios de valores

--- src/roboto/Hombre.java
+++ src/roboto/Hombre.java
@@ -11,8 +11,8 @@ package roboto;
  */
 class Hombre {
     public void jugarConRobot(Robot robot) {
-        robot.avanzar(500);
-        robot.retroceder(20);
+        robot.avanzar(400);
+        robot.retroceder(30);
         System.out.println("Energía actual del robot: " + robot.energiaActual());
         robot.dormir();
     }
